a75556b10e49891adf66262a13842fd25d59098c
PhilippeInCode <felipechacon.montero@gmail.com>
2025-04-29 21:18:34 +0200

    feat: add User, Garment, Photo, Like, PrivateCatalog and PendingGarment models

--- app/Models/PendingGarment.php
+++ app/Models/PendingGarment.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class PendingGarment extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'garment_id',
+        'status',
+        'submitted_at',
+    ];
+
+    public $timestamps = false; 
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function garment()
+    {
+        return $this->belongsTo(Garment::class);
+    }
 }
