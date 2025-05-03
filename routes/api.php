0794b6254f6050b418e4a9f2fbc452a1bbdaeac3
PhilippeInCode <felipechacon.montero@gmail.com>
2025-05-03 18:51:22 +0200

    feat: install and configure Sanctum for API authentication

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\GarmentController;
 
-Route::apiResource('garments', GarmentController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('garments', GarmentController::class);
+});
+
+Route::get('garments',            [GarmentController::class, 'index']);
+Route::get('garments/{garment}',  [GarmentController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('garments',           [GarmentController::class, 'store']);
+    Route::put('garments/{garment}',  [GarmentController::class, 'update']);
+    Route::delete('garments/{garment}', [GarmentController::class, 'destroy']);
+});
