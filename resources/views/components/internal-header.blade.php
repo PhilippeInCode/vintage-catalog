5888831320a8338ba8392e3ad3975463dfbdd901
Felipe <felipechacon.montero@gmail.com>
2025-05-16 11:46:01 +0200

    style(admin.dashboard): resize text sizes

--- resources/views/components/internal-header.blade.php
+++ resources/views/components/internal-header.blade.php
@@ -1,10 +1,10 @@
 <header class="bg-brown text-ivory p-4 flex justify-between items-center shadow-md">
     <div class="flex items-center space-x-4">
         <a href="{{ url('/') }}" class="flex items-center space-x-2">
-            <img src="https://res.cloudinary.com/dk1g12n2h/image/upload/v1747037641/ncpuxn9vrfbr0gmdqezy.png" alt="Logo" class="h-8">
-            <span class="text-lg font-semibold">Vintage Catalog</span>
+            <img src="https://res.cloudinary.com/dk1g12n2h/image/upload/v1747037641/ncpuxn9vrfbr0gmdqezy.png" alt="Logo" class="h-15">
+            <span class="text-xl font-semibold">Vintage Catalog</span>
         </a>
-        <span class="text-sm bg-beige text-brown px-2 py-1 rounded capitalize">
+        <span class="text-xl bg-beige text-brown px-2 py-1 rounded capitalize">
             {{ Auth::user()->role }}
         </span>
     </div>
